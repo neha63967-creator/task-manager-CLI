a05d6463a3d4b00f29084dc78a94fc19f375dd5e
shahid <shahid.64129@iqra.edu.pk>
2025-12-18 20:58:02 +0500

    Add delete-task feature by Shahid

--- task_manager.java
+++ task_manager.java
@@ -5,7 +5,32 @@ public class task_manager {
     // Feature methods will be added by team members
     public static void addTask(String task) {}
     public static void showTasks() {}
-    public static void deleteTask(String task) {}
+   public static void deleteTask(String taskToDelete) {
+    try {
+        java.io.File input = new java.io.File("tasks.txt");
+        java.io.File temp = new java.io.File("temp.txt");
+
+        Scanner sc = new Scanner(input);
+        java.io.PrintWriter pw = new java.io.PrintWriter(temp);
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (!line.equalsIgnoreCase(taskToDelete)) {
+                pw.println(line);
+            }
+        }
+
+        sc.close();
+        pw.close();
+
+        input.delete();
+        temp.renameTo(input);
+
+        System.out.println("Task deleted.");
+    } catch (Exception e) {
+        System.out.println("Error deleting task.");
+    }
+}
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
