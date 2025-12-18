ce4636e7adce8a8cf9411b6e1d0eb131a68650eb
Neha63967 <neha.63967@iqra.edu.pk>
2025-12-18 18:27:05 +0500

    Initial commit structure

---
+++ task_manager.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class task_manager {
+
+    // Feature methods will be added by team members
+    public static void addTask(String task) {}
+    public static void showTasks() {}
+    public static void deleteTask(String task) {}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Task Manager CLI ===");
+        System.out.println("1. Add Task");
+        System.out.println("2. Show Tasks");
+        System.out.println("3. Delete Task");
+
+        System.out.print("Choose option: ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        if (choice == 1) {
+            System.out.print("Enter task: ");
+            addTask(sc.nextLine());
+        } 
+        else if (choice == 2) {
+            showTasks();
+        } 
+        else if (choice == 3) {
+            System.out.print("Enter task to delete: ");
+            deleteTask(sc.nextLine());
+        } 
+        else {
+            System.out.println("Invalid option");
+        }
+    }
+}
